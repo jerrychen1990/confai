0f1c6d02e9b4bc3e6e3c14e4762419b0842bb54e
chenhao <jerrychen1990@gmail.com>
2022-11-22 16:48:42 +0800

    fix confai path

--- confai/__init__.py
+++ confai/__init__.py
@@ -14,6 +14,12 @@ from confai.utils import read_config
 
 __version__ = "0.0.1"
 
+if "CONFAI_PATH" not in os.environ or True:
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    confai_path = os.path.dirname(cur_path)
+    # print(confai_path)
+    os.environ["CONFAI_PATH"] = confai_path
+
 logging_file_name = "logging.dev.yml" if eval(os.environ.get("CONFAI_DEBUG", "False")) else "logging.yml"
 cur_path = os.path.dirname(os.path.abspath(__file__))
 logging_file_name = os.path.join(cur_path, logging_file_name)
